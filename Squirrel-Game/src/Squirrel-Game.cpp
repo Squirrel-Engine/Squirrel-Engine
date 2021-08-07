ca98aed0698c5260a63e561d2cfa647c37a05746
daxPale <canerkurtnazar@gmail.com>
2021-08-08 01:21:53 +0300

    Read me

--- Squirrel-Game/src/Squirrel-Game.cpp
+++ Squirrel-Game/src/Squirrel-Game.cpp
@@ -22,26 +22,30 @@ Application* CreateApplication()
 
 void Sandbox::Run()
 {
+	//Set custom control schema
 	auto testSchema = new TestControlSchema();
 	getInterface<IP_Interface>().setControlSchema(testSchema);
 	
+	//Create a actor via Camera class to make it a First Person Player
 	auto Player = LevelStore::createActor<Camera>(EActorType::CAMERA, "Player");
 	Player->lightComponent.setLightType(ELightType::SpotLight);
 
+	//Create a game object and set its transformation
+	auto Statue = LevelStore::createActor<Skeleton>(EActorType::ACTOR, "Statue");
+	Statue->transformComponent.setTransform(18, -3, 17);
+	Statue->transformComponent.setScale(0.03, 0.03, 0.03);
+	Statue->renderComponent.C_ShaderID = 0;		//Shader
+	Statue->renderComponent.C_ModelID = 2;		//Model
+	Statue->audioComponent.C_AudioSourceID = 0;	//Audio Source
+
 	auto Map = LevelStore::createActor<Skeleton>(EActorType::ACTOR, "Map");
-	Map->transformComponent.setTransform(0,-5, 0);
+	Map->transformComponent.setTransform(0, -5, 0);
 	Map->transformComponent.setScale(0.05, 0.05, 0.05);
 	Map->renderComponent.C_ShaderID = 0;
 	Map->renderComponent.C_ModelID = 3;
 	Map->audioComponent.C_AudioSourceID = 0;
-	
-	auto Statue = LevelStore::createActor<Skeleton>(EActorType::ACTOR, "Statue");
-	Statue->transformComponent.setTransform(18, -3, 17);
-	Statue->transformComponent.setScale(0.03, 0.03, 0.03);
-	Statue->renderComponent.C_ShaderID = 0;
-	Statue->renderComponent.C_ModelID = 2;
-	Statue->audioComponent.C_AudioSourceID = 0;
 
+	//Add light sources to the scene
 	auto FirstLight = LevelStore::createActor<Light>(EActorType::LIGHT, "FirstLight");
 	FirstLight->transformComponent.setTransform(0, 5, 35);
 
